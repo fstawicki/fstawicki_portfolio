685e5eb1b45c6211d23b8ecd934b1c08fd0e63b0
fstawicki <stawickifilip1@gmail.com>
2022-08-11 12:39:21 +0200

    working on contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,30 @@ import { StyledContact } from './styles/Contact.styled'
 const Contact = () => {
   return (
     <StyledContact>
-      
+      <h3 className='color-white'>Get in touch!</h3>
+      <div className="wrapper">
+        <div className="flexDiv">
+          <div className="contactDiv">
+            <p className='color-white'>Personal Data</p>
+            <ul>
+              <li><span className='color-accent'>Name: </span>Filip Stawicki</li>
+              <li><span className='color-accent'>Date of birth: </span>18.10.1998</li>
+              <li><span className='color-accent'>Address: </span>Ożarów Mazowiecki</li>
+              <li><span className='color-accent'>Ability to work: </span> After 1 month - period of notice</li>
+            </ul>
+          </div>
+          <div className="linksDiv">
+            <p className='color-white'>Contact me here</p>
+            <ul className="links">
+              <li><span className='color-accent'>Telephone: </span>507483701</li>
+              <li><span className='color-accent'>Email: </span>stawicki.filip1@gmail.com</li>
+              <li><span className='color-accent'>Github: </span><a href="https://github.com/fstawicki">fstawicki</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </StyledContact>
   )
 }
 
-export default Contact
+export default Contact;
--- src/components/styles/Contact.styled.jsx
+++ src/components/styles/Contact.styled.jsx
@@ -2,8 +2,53 @@ import styled from "styled-components";
 
 export const StyledContact = styled.div`
 
-height: 100vh;
 background-color: ${({theme}) => theme.bgcolor};
+
+.color-white{
+    color: white;
+}
+
+.color-accent{
+    color: ${({theme}) => theme.accent};
+}
+
+ul{
+    list-style: none;
+}
+
+h3{
+    font-size: 3rem;
+    text-align: center;
+    padding: 8rem 0;
+}
+
+.wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.flexDiv{
+    display: flex;
+    
+}
+
+.flexDiv p{
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
+}
+
+.flexDiv li{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+    color: ${({theme}) => theme.greytext};
+}
+
+.linksDiv{
+    margin-left: 10rem;
+}
+
 
 
 ` 
