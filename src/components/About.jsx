16e966055ae6e78eeda64f1bfb248223c526f3ef
fstawicki <stawickifilip1@gmail.com>
2023-09-25 18:09:04 +0200

    changed paragraph in about, changed cvs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <StyledAbout>
       <div className="infoText">
         <h3 className="aboutme">About Me:</h3>
-        <p>I completed my masters studies in Computer Science at the Faculty of Applied Informatics and Mathematics at the Warsaw University of Life Sciences. I have been working as an IT specialist since 2019. My professional goal is to work as a Front-end Developer. I am interested in React, in which I am constantly improving. My native language is Polish. I also speak English at C1 level.</p>
+        <p className='whitecolor'>I completed my masters studies in Computer Science at the Faculty of Applied Informatics and Mathematics at the Warsaw University of Life Sciences. I have been working as an IT specialist since 2019. My professional goal is to work as a Front-end Developer. I am interested in React, in which I am constantly improving. I worked on many projects with a team, where I aquired experience in testing, team work, code reviews, git and CI/CD. My native language is Polish and I also speak English at C1 level.</p>
       </div>
       <div className="imageDiv">
         <img src={imageFilip} alt="Filip Stawicki" className="myImg" />
